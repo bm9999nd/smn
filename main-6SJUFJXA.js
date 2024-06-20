afc856a75c380469793c5bbf5741fa586ef7de33
nda_666 <bm9999nd@gmail.com>
2024-06-20 22:43:03 +0700

    Auto-generated commit

---
+++ main-6SJUFJXA.js
@@ -0,0 +1,1 @@
+import{I as t,J as p,K as r,L as e,M as i}from"./chunk-ZTVGFMBD.js";var m=[{path:"",loadComponent:()=>import("./chunk-MO7UQUHL.js").then(o=>o.KasComponent)}];var n={providers:[e(m),t(p())]};r(i,n).catch(o=>console.error(o));
